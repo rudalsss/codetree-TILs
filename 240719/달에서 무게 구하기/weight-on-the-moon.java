86d997b774ad850016f597bcff75b2b2c934a49f
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-19 10:39:55 +0900

    Title: 달에서 무게 구하기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240719/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double b = 0.165;
+        double c = a*b;
+        System.out.printf("13 * 0.165000 = %.6f", c);
+    }
+}
