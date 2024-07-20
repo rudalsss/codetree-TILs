4dedc5c6b347a8488ce7186a059a9897ac57be25
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-20 13:08:06 +0900

    Title: 숫자의 조건 여부, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240720/숫자의 조건 여부/number's-condition.java
+++ 240720/숫자의 조건 여부/number's-condition.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
-        if(a>113){
+        if(a>=113){
             System.out.println(1);
         } else {
             System.out.println(0);
