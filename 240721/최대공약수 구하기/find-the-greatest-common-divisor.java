e90701a678883a456907c68819bf22360278bf56
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-21 20:56:13 +0900

    Title: 최대공약수 구하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(gcd(a, b));
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
