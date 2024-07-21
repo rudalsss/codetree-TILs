56049ab4ca32997095ba54b72674c9cb7ff64146
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-21 14:46:47 +0900

    Title: 특정 조건 세 정수 비교, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240721/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min = a;
+        if(min<b) min=b;
+        if(min<c) min=c;
+
+        System.out.print(a==min?1:0);
+        System.out.print(' ');
+        System.out.println(a==b && b==c && a==c ?1:0);
+    }
+}
