35e48df7cca7ae7c8e3919e6b6b69a75a80899f1
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-22 23:48:28 +0900

    Title: 두 정수 값 교환하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/두 정수 값 교환하기/to-exchange-two-integer-values.java
+++ 240722/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -6,7 +6,7 @@ public class Main {
         int temp = n;
         n = m;
         m = temp;
-        return new int[]{n, m};
+        return new int[]{m, n};
     }
 
     public static void main(String[] args) {
