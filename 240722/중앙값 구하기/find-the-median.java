4d361bd3195c09616b474f69601e1ac448109592
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-22 22:06:37 +0900

    Title: 중앙값 구하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240722/중앙값 구하기/find-the-median.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int mid = a;
+
+        if((b<mid && c<b)||(b>mid && b<c )) mid = b;
+        if((c<mid && a<c)||(c>mid && c<a )) mid = c;
+
+        System.out.println(mid);
+    }
+}
