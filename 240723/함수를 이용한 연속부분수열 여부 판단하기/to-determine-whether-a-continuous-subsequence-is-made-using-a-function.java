58b2848f1e7a2d864f3532aad52db14344edad82
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-23 22:24:54 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240723/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -22,31 +22,26 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // 입력받기 위한 Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
 
-        // 수열 A의 크기 입력받기
         int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        
         int[] A = new int[n1];
         for (int i = 0; i < n1; i++) {
             A[i] = sc.nextInt();
         }
 
-        // 수열 B의 크기 입력받기
-        int n2 = sc.nextInt();
+        
         int[] B = new int[n2];
         for (int i = 0; i < n2; i++) {
             B[i] = sc.nextInt();
         }
 
-        // 수열 B가 수열 A의 연속 부분 수열인지 확인
         if (isSubsequence(A, B)) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
-
-        // Scanner 객체 닫기
-        sc.close();
     }
 }
