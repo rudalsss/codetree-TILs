fbf608cf3712a91d342990dd2d8ffc6a5631dfff
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-23 19:40:56 +0900

    Title: 100점 도달하기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240723/100점 도달하기/reach-100-point.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void score(int a){
+        if(a>=90){
+            System.out.print("A ");
+        } else if(a>=80){
+            System.out.print("B ");
+        } else if(a>=70){
+            System.out.print("C ");
+        } else if(a>=60){
+            System.out.print("D ");
+        } else {
+            System.out.print("F");
+        }
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        while(a<=100){
+            score(a);
+            a++;
+        }
+
+        
+
+    }
+}
