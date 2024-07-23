58655a0810ee3f8c2fb542bab5f5347532512fcd
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-23 22:29:48 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean chk(int a, int b){
+        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if( b<=days[a+1] ){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(chk(a,b)){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+    }
+}
