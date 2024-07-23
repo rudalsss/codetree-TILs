51022efe35bb07306bf6cc30f637739c41f44432
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-23 22:48:20 +0900

    Title: 그 계절, 그 날, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240723/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void chk( int a, int b, int c ){
+        int[] arr = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if( ( a%4==0 && a%100!=0 ) || ( a%4==0 && a%100==0 && a%400==0 ) ){
+            arr[1] = 29;
+        }
+
+        if( (b>=1 && b<=12) && (c>=1 && c<=arr[b-1]) ){
+            if(b>=3&&b<=5){
+                System.out.println("Spring");
+            } else if (b>=6 && b<=8){
+                System.out.println("Summmer");
+            } else if(b>=9 && b<=11){
+                System.out.println("Fall");
+            } else {
+                System.out.println("Winter");
+            }
+
+        } else {
+            System.out.println(-1);
+        }
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();//년
+        int b = sc.nextInt();//월
+        int c = sc.nextInt();//일
+
+        chk(a,b,c);
+
+
+    }
+}
