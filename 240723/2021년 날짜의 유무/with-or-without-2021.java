87970bf2c57e9fd7ecbdc267094ec06e68f1b1ea
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-23 22:33:44 +0900

    Title: 2021년 날짜의 유무, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/2021년 날짜의 유무/with-or-without-2021.java
+++ 240723/2021년 날짜의 유무/with-or-without-2021.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
-    public static boolean chk(int a, int b){
+    public static boolean chk(int m, int d) {
+        // 각 월의 일수를 배열에 저장
         int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        if( b<=days[a-1] ){
-            return true;
-        }else {
+        
+        // 월이 유효 범위 내에 있는지 확인
+        if (m < 1 || m > 12) {
             return false;
         }
+
+        // 해당 월의 일수가 유효 범위 내에 있는지 확인
+        if (d < 1 || d > days[m - 1]) {
+            return false;
+        }
+        
+        return true;
     }
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
 
-        if(chk(a,b)){
+        if (chk(m, d)) {
             System.out.println("Yes");
-        }else {
+        } else {
             System.out.println("No");
         }
+
     }
 }
