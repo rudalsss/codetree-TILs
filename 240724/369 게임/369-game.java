744ca62d95407ca9c7c47598be4305a4cd8c3fc9
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-24 10:13:46 +0900

    Title: 369 게임, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240724/369 게임/369-game.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean chk(int n){
+        while(n>0){
+            n /= 10;
+            if( n%10 == 3 || n%10 == 6 || n%10 == 9 ){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for(int i=1; i<=a; i++){
+            if( i%3==0 || chk(i)){
+                System.out.print(0+" ");
+            }else {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
