c79f55929d18601e019e4e977b0511bf61bcb933
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-24 10:30:01 +0900

    Title: 청소당번 정하기, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240724/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int d = a/12;
+        int b = a/2 - 1 -d;
+        int c = a/3 - d;
+        System.out.print(b+" "+c+" "+d);
+    }
+}
