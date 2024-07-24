41ec2fa7f7f67a7343b42a67a886018a6c6b9a06
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-24 13:30:33 +0900

    Title: 최소공배수 구하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240724/최소공배수 구하기/find-the-least-common-multiple.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int gcd(int a, int b){
+        while(b!=0){
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(n*m/gcd(n,m));
+        
+    }
+}
