6b479bc7e8afd28403b6d0d693ddd20c7f2384e1
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-24 10:34:18 +0900

    Title: 청소당번 정하기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240724/청소당번 정하기/cleaning-numbering.java
+++ 240724/청소당번 정하기/cleaning-numbering.java
@@ -8,8 +8,8 @@ public class Main {
         int a = sc.nextInt();
 
         int d = a/12;
-        int b = a/2 - 1 -d;
-        int c = a/3 - d;
+        int b = a/2 -d -a/6;
+        int c = a/3 -d;
         System.out.print(b+" "+c+" "+d);
     }
 }
