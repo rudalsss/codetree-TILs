a26e21f13d5be2a3029b18b541b389d83ee8a572
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-07-25 20:18:19 +0900

    Title: 코드네임, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240725/코드네임/code-name.java
+++ 240725/코드네임/code-name.java
@@ -5,16 +5,19 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        
-
         A arr[] = new A[5];
-        int idx = 0;
-        int score = 100;
+        
         for(int i=0; i<5; i++){
             char a = sc.next().charAt(0);
             int b = sc.nextInt();
             arr[i] = new A(a,b);
-            if(arr[i].score<score){
+        }
+
+        int idx = 0;
+        int score = arr[0].score;
+        for(int i=0; i<5; i++){
+            if(arr[i].score < score){
+                idx = i;
                 score = arr[idx].score;
             }
         }
