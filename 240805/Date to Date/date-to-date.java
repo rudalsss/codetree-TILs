c38a526bcd59e54bd458db96496ed09db45abc60
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-08-05 17:36:42 +0900

    Title: Date to Date, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240805/Date to Date/date-to-date.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for(int i=0; i<=a;i++) sum1 += days[i];
+        sum1 += b;
+
+        for(int i=0; i<=c; i++) sum2 += days[i];
+        sum2 += d;
+
+        System.out.println(sum2-sum1);
+
+        
+
+    }
+}
