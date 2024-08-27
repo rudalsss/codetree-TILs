fcaa159ba5b0801a1cfc5e5a1c4131b0a056516d
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-08-28 00:45:31 +0900

    Title: G or H 3, Time: 183ms, Memory: 11MB, Status: Passed - CodeTree

--- 240828/G or H 3/G-or-H-3.java
+++ 240828/G or H 3/G-or-H-3.java
@@ -29,7 +29,7 @@ public class Main {
 
 
         int maxScore = 0;
-        for(int i=1; i<=m-k+1; i++){
+        for(int i=1; i<=m; i++){
             int sumScore = 0;
             for(int j=i; j<=i+k && j<=m; j++){
                 sumScore += placeScore[j];
